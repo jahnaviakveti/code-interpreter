c2cd1aecd45aadff6595bcfaffe1c44483d684e1
Jahnavi Akveti <jahnavisrisai.akveti@gmail.com>
2026-06-18 13:25:11 +0530

    Enable CORS

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import sys
 from io import StringIO
@@ -6,6 +7,14 @@ import traceback
 import re
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class CodeRequest(BaseModel):
     code: str
